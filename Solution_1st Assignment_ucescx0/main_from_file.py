5cb5bf717d09b9b65050c384c4bb3ecdeb028be9
ccx_github <1617291522@qq.com>
2020-11-16 02:40:16 +0800

    read input.csv completed

--- Solution_1st Assignment_ucescx0/main_from_file.py
+++ Solution_1st Assignment_ucescx0/main_from_file.py
@@ -5,8 +5,26 @@ def main():
     plotter = Plotter()
     print("read polygon.csv")
 
+
+
     print("read input.csv")
 
+    px = []
+    py = []
+    i = 0
+    ind = []
+    with open('E:/0096_1st_assignment/Solution_1st Assignment_ucescx0/input.csv', 'r') as f:
+        line = f.readline()
+        while line:
+            # print(line)
+            line = f.readline()
+            pt = line.split(',')
+            # print(pt)
+            if line != '':
+                ind.insert(i, pt[0])
+                px.insert(i, float(pt[1]))
+                py.insert(i, float(pt[2]))
+                i = i + 1
     print("categorize points")
 
     print("write output.csv")
