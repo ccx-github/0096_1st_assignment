4457c5af76df25007e0861f91fd7673773c7a390
ccx_github <1617291522@qq.com>
2020-11-16 03:03:51 +0800

    read polygon.csv completed

--- Solution_1st Assignment_ucescx0/main_from_file.py
+++ Solution_1st Assignment_ucescx0/main_from_file.py
@@ -4,26 +4,35 @@ from plotter import Plotter
 def main():
     plotter = Plotter()
     print("read polygon.csv")
-
+    plgnx = []
+    plgny = []
+    ind1 = []
+    with open('E:/0096_1st_assignment/Solution_1st Assignment_ucescx0/polygon.csv', 'r') as f: # read polygon.csv
+        line1 = f.readline()
+        while line1:  # extract every row in the CSV
+            line1 = f.readline()
+            plgn = line1.split(',')  # split strings with comma
+            if line1 != '':
+                ind1.insert(j, plgn[0])
+                plgnx.insert(j, float(plgn[1]))  # get x cooridinate lists
+                plgny.insert(j, float(plgn[2]))  # get y cooridinate lists
+                j = j + 1
 
 
     print("read input.csv")
 
     px = []
     py = []
-    i = 0
-    ind = []
-    with open('E:/0096_1st_assignment/Solution_1st Assignment_ucescx0/input.csv', 'r') as f:
+    ind2 = []
+    with open('E:/0096_1st_assignment/Solution_1st Assignment_ucescx0/input.csv', 'r') as f: # read input.csv
         line = f.readline()
-        while line:
-            # print(line)
+        while line: # extract every row in the CSV
             line = f.readline()
-            pt = line.split(',')
-            # print(pt)
+            pt = line.split(',') # split strings with comma
             if line != '':
-                ind.insert(i, pt[0])
-                px.insert(i, float(pt[1]))
-                py.insert(i, float(pt[2]))
+                ind2.insert(i, pt[0])
+                px.insert(i, float(pt[1])) # get x cooridinate lists
+                py.insert(i, float(pt[2])) # get y cooridinate lists
                 i = i + 1
     print("categorize points")
 
