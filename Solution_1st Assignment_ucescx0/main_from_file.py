1dd42e368a6e3ef87ea905b62189447f61be01c0
ccx_github <1617291522@qq.com>
2020-11-16 18:32:20 +0800

    y_min coordinate of polygon still error. MBR user have been completed. The error have been solved by importing sort.

--- Solution_1st Assignment_ucescx0/main_from_file.py
+++ Solution_1st Assignment_ucescx0/main_from_file.py
@@ -1,14 +1,16 @@
+from numpy import sort   # for sorting
 from plotter import Plotter
 import matplotlib.pyplot as plt
 
+
 def main():
     plotter = Plotter()
-    # print("read polygon.csv")
+
+    print("read polygon.csv")
     plgnx = []
     plgny = []
     ind1 = []
     j = 0
-
     with open('E:/0096_1st_assignment/Solution_1st Assignment_ucescx0/polygon.csv', 'r') as f: # read polygon.csv
         line1 = f.readline()
         while line1:  # extract every row in the CSV
@@ -19,10 +21,9 @@ def main():
                 plgnx.insert(j, float(plgn[1]))  # get x coordinate lists of vertexes
                 plgny.insert(j, float(plgn[2]))  # get y coordinate lists of vertexes
                 j = j + 1
-    print(plgnx)
-    print(plgny)
-    plt.fill(plgnx, plgny, 'lightgrey')
-    # print("read input.csv")
+    plt.fill(plgnx, plgny, 'lightgrey')  # fill the polygon
+
+    print("read input.csv")
     px = []
     py = []
     ind2 = []
@@ -37,10 +38,17 @@ def main():
                 px.insert(i, float(pt[1])) # get x coordinate lists of inputs
                 py.insert(i, float(pt[2])) # get y coordinate lists of inputs
                 i = i + 1
+
     print("categorize points")
-
     lt = []
+    # import sort to make the code easier
 
+    def get_extremum(lt):  # get extremum of a list
+        lt = sort(lt)  # sort from small to big
+        lt_max = lt[len(lt) - 1]  # get the maximum
+        lt_min = lt[0]  # get the minimum
+        return lt_max, lt_min
+    ''' # this method is complex
     def get_max(lt):  # get maximum of a list
         for k in range(len(lt)-1):
             if lt[k] >= lt[k + 1]:
@@ -59,31 +67,35 @@ def main():
                 lt_min = lt[t+1]
         # print(plgnx_min)
         return lt_min
+    '''
 
     def mbr_1(p, pn_max, pn_min):  # MBR execute
         if p > pn_max or p < pn_min:
             return 'outside'
         else:
-            return 'inside'
+            return ''
 
-    print('''Do you want to use which algorithm to judge PIP?
+    print('''Do you want to use which method to judge PIP?
        if MBR,input the number :1
-       if RCA,input the number :2''')  # start
+       if RCA,input the number :2''')  # start the code
     n_input = int(input('The number you put:'))
     l = 0
-    x_max = get_max(plgnx)  # get maximum x coordinate of vertexes in polygon
-    x_min = get_min(plgnx)  # get minimum x coordinate of vertexes in polygon
-    y_max = get_max(plgny)  # get maximum y coordinate of vertexes in polygon
-    y_min = get_min(plgny)  # get minimum y coordinate of vertexes in polygon
-    plt.plot([x_max,x_max,x_min,x_min,x_max],[y_max,y_min,y_min,y_max,y_max])
+    x_max, x_min = get_extremum(plgnx)  # get maximum/minimum x coordinate of vertexes in polygon
+    y_max, y_min = get_extremum(plgny)  # get maximum/minimum y coordinate of vertexes in polygon
+    # this method change the value of processing, make error
+    #  x_max = get_max(plgnx)  # get maximum x coordinate of vertexes in polygon
+    # x_min = get_min(plgnx)  # get minimum x coordinate of vertexes in polygon
+    # y_max = get_max(plgny)  # get maximum y coordinate of vertexes in polygon
+    # y_min = get_min(plgny)  # get minimum y coordinate of vertexes in polygon
+    plt.plot([x_max,x_max,x_min,x_min,x_max],[y_max,y_min,y_min,y_max,y_max])# plot the mbr
     if n_input == 1:
         for l in range(len(px)):
             res1 = mbr_1(px[l], x_max, x_min)
             res2 = mbr_1(py[l], y_max, y_min)
-            if res1 == res2 and res1 == 'inside':
-                plotter.add_point(px[l], py[l], 'inside')  # add points by x coordinate to the plotter
+            if res1 == 'outside' or res2 == 'outside':
+                plotter.add_point(px[l], py[l], 'outside')
             else:
-                plotter.add_point(px[l], py[l], 'outside')  # add points by y coordinate to the plotter
+                plotter.add_point(px[l], py[l], '')
         plotter.add_polygon(plgnx, plgny)
         plt.fill(plgnx, plgny,'lightgrey')
 
@@ -91,7 +103,7 @@ def main():
     else:
         print('The number that you have input is worry.please input again after reading carefully!')
     print("write output.csv")
-    # print("plot polygon and points")
+    print("plot polygon and points")
     plotter.show()
 
 
